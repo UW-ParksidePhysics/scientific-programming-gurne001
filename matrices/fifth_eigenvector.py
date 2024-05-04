201a14b1538ebe3cf25d2f4438400206ccb32dd1
Tyler <gurne001@rangers.uwp.edu>
2024-05-04 15:12:13 -0500

    Adding Matrices assignment

---
+++ matrices/fifth_eigenvector.py
@@ -0,0 +1,27 @@
+import numpy, matplotlib
+from numpy import *
+from matplotlib import pyplot
+off_diagonal = -1*numpy.ones(4)
+upper_diagonal = numpy.diag(off_diagonal, k=1)
+lower_diagonal = numpy.diag(off_diagonal, k=-1)
+diagonal = numpy.diag((2*numpy.ones(5)))
+A = upper_diagonal + lower_diagonal + diagonal
+
+coefficient = 1/(2*((1/6)**2))
+
+H = coefficient*A
+
+eigenvalues, eigenvectors = numpy.linalg.eig(H)
+print(eigenvectors)
+x_values =numpy.linspace(1/6, 5/6, 5)
+
+def comparison_function(x):
+    f = sqrt(2)*sin(pi*x)
+    return f
+
+comparison_x_values = numpy.linspace(0, 1, 50)
+comparison_y_values = comparison_function(comparison_x_values)
+
+matplotlib.pyplot.plot(x_values, eigenvectors[4])
+matplotlib.pyplot.plot(comparison_x_values, comparison_y_values)
+matplotlib.pyplot.show()
---
+++ matrices/tenth_eigenvector.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot
+import numpy
+from numpy import *
+from matplotlib import pyplot
+
+matrix_dimension = 10
+
+off_diagonal = -1*numpy.ones((matrix_dimension-1))
+upper_diagonal = numpy.diag(off_diagonal, k=1)
+lower_diagonal = numpy.diag(off_diagonal, k=-1)
+diagonal = numpy.diag(2*numpy.ones(matrix_dimension))
+A = upper_diagonal + lower_diagonal + diagonal
+
+coefficient = 1/(2*((1/matrix_dimension)**2))
+
+H = coefficient*A
+
+eigenvalues, eigenvectors = numpy.linalg.eig(H)
+
+x_values = numpy.linspace(1/(matrix_dimension+1), matrix_dimension/(matrix_dimension+1), matrix_dimension)
+
+
+def comparison_function(x):
+    f = sqrt(2)*sin(pi*x)
+    return f
+
+
+comparison_x_values = numpy.linspace(0, 1, 50)
+comparison_y_values = comparison_function(comparison_x_values)
+
+matplotlib.pyplot.plot(comparison_x_values, comparison_y_values)
+matplotlib.pyplot.plot(x_values, eigenvectors[9])
+matplotlib.pyplot.show()
